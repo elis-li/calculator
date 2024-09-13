ef0c32b5523a9047b03daf0fa1e23f72b376ba7b
elis-li <petrushina.2014@mail.ru>
2024-09-13 11:25:24 +0300

    2

--- lib/Keyboard/keyboard.dart
+++ lib/Keyboard/keyboard.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
 
 class Keyboard extends StatelessWidget{
+  const Keyboard({super.key});
+
+  @override
+  Widget build(BuildContext context
+      ) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
 
 }
