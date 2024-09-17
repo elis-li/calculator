3c3d279e3ba6da6b2a596dd5a3639296331bedd1
elis-li <petrushina.2014@mail.ru>
2024-09-17 16:29:44 +0300

    3

--- lib/CalculatorApp/CalculatorApp.dart
+++ lib/CalculatorApp/CalculatorApp.dart
@@ -7,6 +7,15 @@ class CalculatorView extends StatefulWidget{
   @override
   State<CalculatorView> createState() => _CalculatorViewState();
 }
+String equation = "0";
+  String result = "0";
+  String expression = "0";
+  double equationFontSize = 38.0;
+  double resultFontSize = 48.0;
+
+  buttonPressed(String buttonText){
+
+  }
 
 class _CalculatorViewState extends State<CalculatorView> {
   @override
@@ -16,9 +25,44 @@ class _CalculatorViewState extends State<CalculatorView> {
         appBar: AppBar(
           elevation: 0,
           backgroundColor: Colors.indigo,
-          leading: const Icon(Icons.settings, color: Colors.indigo),
+          leading: const Icon(Icons.settings, color: Colors.black87),
+          actions: const [
+            Padding(
+                padding: EdgeInsets.only(top: 0),
+                child: Text('DEG')),
+            SizedBox(width: 22)
+          ],
           title: const Text ('Calculator'),
         centerTitle: true,
-      ));
+      ),
+    body: const SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Align(
+              alignment: Alignment.bottomRight,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text('result',
+                          textAlign: TextAlign.left,
+                          style: const TextStyle(
+                            color: Colors.white, fontSize: 80))),
+
+                          ) ,
+                      ),
+                    )
+                  ],
+                ),
+
+              ),
+            )
+          ],
+        ),
+    ),);
   }
 }
